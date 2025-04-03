cd33d100a82b4b965f50e7fffc42faeccafcde03
IvanDelibaltov <168005200+IvanDelibaltov@users.noreply.github.com>
2025-04-03 19:29:52 +0300

    constant.py

--- lab_1/constant.py
+++ lab_1/constant.py
@@ -1,1 +1,7 @@
 ALPHABET =  'АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ'
+INPUT_TEXT_FILE = "code6.txt"
+FREQ_ANALYSIS_FILE = "freq_analys.json"
+FREQ_RUSSIAN_FILE = "freq_russian.json"
+DECRYPTION_KEY_FILE = "key2.json"
+DECRYPTED_TEXT_OUTPUT_FILE = "decrypted_text_output.txt"
+CODE6_DECRYPTED_FILE = "code6decrypted.txt"
