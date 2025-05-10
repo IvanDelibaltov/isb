fe594065754fed4d3d724bdbabeaca592690c47f
IvanDelibaltov <168005200+IvanDelibaltov@users.noreply.github.com>
2025-05-10 16:09:37 +0300

    add constants

---
+++ consts.py
@@ -0,0 +1,8 @@
+cpp_sequence_txt = "../FIRST/CPP_bits.txt"
+
+java_sequence_txt = "../FIRST/JAVA_bits.txt"
+
+results_cpp = "test_results_cpp.txt"
+results_java = "test_results_java.txt"
+
+PI = [0.2148, 0.3672, 0.2305, 0.1875]
